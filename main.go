2e7c02556ee91f051016bb2f4daf787ecfd1c1c8
brennerlemes <brennerdepaulalemes@gmail.com>
2026-08-30 23:12:15 -0300

    implementa posicao

--- main.go
+++ main.go
@@ -1,27 +1,14 @@
-func (l *lista) removerPosicao(posicao int) (int, bool) {
-	if posicao < 0 || l.inicio == nil {
-		return 0, false
-	}
+func (l *lista) posicao(valorProcurado int) (int, bool) {
+	atual := l.inicio
+	indice := 0
 
-	if posicao == 0 {
-		valor := l.inicio.valor
-		l.inicio = l.inicio.proximo
-		return valor, true
-	}
-
-	anterior := l.inicio
-	for i := 0; i < posicao-1; i++ {
-		if anterior.proximo == nil {
-			return 0, false
+	for atual != nil {
+		if atual.valor == valorProcurado {
+			return indice, true
 		}
-		anterior = anterior.proximo
+		atual = atual.proximo
+		indice++
 	}
 
-	if anterior.proximo == nil {
-		return 0, false
-	}
-
-	valor := anterior.proximo.valor
-	anterior.proximo = anterior.proximo.proximo
-	return valor, true
+	return 0, false
 }
